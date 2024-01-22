f1311cbc838d9458e5368f97c50c5eb4121d0ce1
renukanusha <renukanusha1026@gmail.com>
2024-01-23 00:06:23 +0530

    saved as new file

---
+++ changecolorapp/src/App.js
@@ -0,0 +1,23 @@
+function App() {
+  const handleclickfunction = (e) => {
+    const body = document.querySelector('body')
+    body.style.background = getRandomColor()
+  };
+
+  function getRandomColor(){
+    let letters = '0123456789ABCDE'
+    let color = '#'
+    for(let i=0; i<6; i++){
+      color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
+  }
+  return (
+    <div className="container">
+      <h1> Random Background Color Changer </h1>
+      <button className="btn" onClick={handleclickfunction} >Click Me</button>
+    </div>
+  );
+}
+
+export default App;
